14be7c369cf94db5e7ad84219f4291b7cc375929
Marchionni Martin <tinchomarchi34@gmail.com>
2024-08-17 19:46:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-        return {
-        define: {
-          __APP_ENV__: process.env.VITE_VERCEL_ENV,
-        },
-      };
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
 })
